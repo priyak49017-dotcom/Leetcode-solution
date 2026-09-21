9883077721092ea3207a6189fc2854117931e7b2
priyak49017-dotcom <priyak49017@gmail.com>
2026-09-21 10:07:53 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (75.82%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -22,16 +22,14 @@ class Solution {
     public int find(TreeNode root){
         
         if(root == null) return 0;
+        //if(root.left == null && root.right == null) return root.val;
         
         int left=Math.max(0,find(root.left));
         int right=Math.max(0,find(root.right));
-    
+        
         max=Math.max(max, left + root.val+ right);
         return root.val+Math.max(left,right);
         
-        
-        
-    
-        
-    }
+        }
 }
+        
