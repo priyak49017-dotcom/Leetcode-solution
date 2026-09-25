bad85564aeca33a904fac473bf07b21350add75a
priyak49017-dotcom <priyak49017@gmail.com>
2026-09-25 11:47:16 +0530

    Time: 1 ms (99.62%) | Memory: 42.9 MB (51.88%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -0,0 +1,22 @@
+class Solution {
+    PriorityQueue<Integer>heap=new PriorityQueue<>(Collections.reverseOrder());
+    public int lastStoneWeight(int[] stones) {
+        for(int stone : stones){
+            heap.add(stone);
+
+        }
+        while(heap.size() > 1){
+            int first=heap.poll();
+            int second=heap.poll();
+            if(first != second){
+                heap.add((first-second));
+            }
+            if(heap.size() == 0){
+                return 0;
+            }
+        }
+        return heap.remove();
+
+        
+    }
+}
