2ae06e6e93b00440afeac459571c1041a4f36839
priyak49017-dotcom <priyak49017@gmail.com>
2026-09-26 18:45:44 +0530

    Time: 15 ms (55.31%) | Memory: 47.9 MB (13.84%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -4,14 +4,11 @@ class Solution {
         for(int num:nums){
             frequency.put(num,frequency.getOrDefault(num,0)+1);
         }
-        PriorityQueue<Integer>heap=new PriorityQueue<>((a,b)->frequency.get(a)-frequency.get(b));
-        for(int num:frequency.keySet()){
-            heap.add(num);
+        PriorityQueue<Integer>heap=new PriorityQueue<>((a,b)->frequency.get(b)-frequency.get(a));
+        heap.addAll(frequency.keySet());
         
-        if(heap.size() > k){
-            heap.poll();
-        }
-        }
+        
+        
         int[] result=new int[k];
         for(int i=0;i<k;i++){
             result[i]=heap.poll();
